2573987b3c969a3ba09ebe4b81db94d6ede779e0
Sung Ho Kim <likeleonursh@hotmail.com>
2015-08-16 13:30:20 +0900

    아이템용 모델, 마이그레이션

--- functional_tests/superlists/functional_test.py
+++ functional_tests/superlists/functional_test.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 사이트 접속
         self.browser.get('http://localhost:8000')
@@ -31,10 +36,18 @@ class NewVisitorTest(unittest.TestCase):
 
         # 엔터키를 치면 페이지가 갱신되고 작업 모록에 아이템이 추가
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: 히오스 하기')
 
+        # 다시 추가
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('히오스에서 1승 하기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 페이지는 갱신되고, 두 개 아이템이 목록에 보인다
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 히오스 하기', [row.text for row in rows])
+        self.check_for_row_in_list_table('2: 히오스에서 1승 하기')
+        self.check_for_row_in_list_table('1: 히오스 하기')
 
         self.fail('Finish the test!')
 
