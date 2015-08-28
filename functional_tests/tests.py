bf61124744949ecdcd458e367fe9346a8f33560c
Sung Ho Kim <likeleon@gmail.com>
2015-08-29 08:26:28 +0900

    레이아웃과 스타일링을 위한 첫 FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -78,3 +78,14 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('히오스 하기', page_text)
         self.assertIn('사진 찍기', page_text)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
